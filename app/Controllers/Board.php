2315f3491e2961571d963640cdbfb18e2d33cba4
leeshahub <leesha1223@naver.com>
2023-03-05 21:05:24 +0900

    route practice

---
+++ app/Controllers/Board.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class Board extends BaseController
+{
+    public function index()
+    {
+        echo "dddddddddddddddddddddddddddddddddddd";
+        exit;
+        return view('index');
+    }
+}
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,8 @@ class Home extends BaseController
 {
     public function index()
     {
+        echo "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        exit;
         return view('welcome_message');
     }
 }
