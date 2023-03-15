7401d7234652eb1412dafd63635a41a39cc1d224
leesha <amazing1124@gmail.com>
2023-03-15 17:47:23 +0900

    Revert "benchmark 기록"
    This reverts commit a4dd0c5fb29ea46cd4ad3818c4c8fd107b76c2e8.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -36,11 +36,9 @@ $routes->setAutoRoute(false);
 /*
 라우팅 공부중
 http://ci4doc.cikorea.net/incoming/routing.html
-벤치마크 공부중
-http://ci4doc.cikorea.net/testing/benchmark.html
+
 CI 포럼
 http://sample4.cikorea.net/
-
 */ 
 
 $routes->get('/', 'Home::index');
