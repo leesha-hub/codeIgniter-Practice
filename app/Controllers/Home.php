07d3a2d24db30e4af895c497f878140db97ee1ff
leeshahub <leesha1223@naver.com>
2023-03-28 21:04:28 +0900

    develop commmit for merge test

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends BaseController
 {
     public function index()
     {
-        echo "master commit";
+        echo "develop commit";
         exit;
         return view('welcome_message');
     }
